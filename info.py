b3c89ae0802ba090bef54c5314f9764ea00767d0
sivaraman444 <119070317+Sivaramanyt@users.noreply.github.com>
2025-03-01 00:00:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -165,8 +165,8 @@ PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
 else:
-    ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+    ON_HEROKU = True 
+URL = environ.get("URL", "https://faint-allegra-rolexsir-7a1ec4b1.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
