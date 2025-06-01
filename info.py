7f1264604a6abebd2bb805b6adb725c74052492b
sivaraman444 <119070317+Sivaramanyt@users.noreply.github.com>
2025-06-01 11:26:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -113,7 +113,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', True))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'smallshorts.com')
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'https://smallshorts.com')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '0bf8bbb8f7baacc740481334d60ef651b45d13bb')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/c/2386462158/7/434')
 
